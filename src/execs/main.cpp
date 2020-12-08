d87cac3ef384ae8f22264c233aa7d6fda9fdd72b
Alois <alois.aubert99@gmail.com>
2020-12-08 21:30:22 +0100

    testBranche
    message pour paul

--- src/execs/main.cpp
+++ src/execs/main.cpp
@@ -2,5 +2,6 @@
 
 int main(){
     std::cout << "Phrase de test pour vous montrer les branches" << std::endl;
+    std::cout <<"Phrase pour remercier Paul du fond du coeur " <<srd::endl;
 return 0;
 }

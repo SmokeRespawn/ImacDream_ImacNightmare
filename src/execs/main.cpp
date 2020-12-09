af015f1f64c6a4aa74860dcba5c9951f9f11482d
Smoke <paulrozaire@gmail.com>
2020-12-09 21:26:35 +0100

    ReInstall
    Ajout de glfw + consignes d'installation pour glfw
    CA COMPILE HALLELUJA

--- src/execs/main.cpp
+++ src/execs/main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <map>
 
-int main(){
-return 0;
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 800
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
